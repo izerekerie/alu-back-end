df208ae3fef24278f872f76a8263d47c7ec5c254
izerekerie <izereuwonkunda@gmail.com>
2024-06-28 11:15:28 +0200

    fix get data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
+"""Module"""
+
 import requests
 import sys
 
-def get_employee_name(employee_id):
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    if response.status_code == 200:
-        return response.json().get('name')
-    else:
-        print(f"Error: Unable to fetch employee details (Status code: {response.status_code})")
-        sys.exit(1)
 
-def get_todo_list(employee_id):
-    response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error: Unable to fetch TODO list (Status code: {response.status_code})")
-        sys.exit(1)
-
-def main(employee_id):
-    employee_name = get_employee_name(employee_id)
-    todo_list = get_todo_list(employee_id)
-
-    total_tasks = len(todo_list)
-    completed_tasks = [task for task in todo_list if task['completed']]
-    number_of_done_tasks = len(completed_tasks)
-
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
+"""Module"""
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(user_id)
 
-    try:
-        employee_id = int(sys.argv[1])
-        main(employee_id)
-    except ValueError:
-        print("Error: The employee ID must be an integer")
-        sys.exit(1)
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
+
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+
+    [print("\t " + task["title"]) for task in task_completed]
